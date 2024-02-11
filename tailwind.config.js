0558e6a25deae9162ce266dc7b771e51e6cbabfd
Abdessamadds <Ahmedbosofa1997@gmail.com>
2024-02-11 09:04:14 +0100

    Hero Section Added with style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,39 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*.{html},js'],
+  content: ['*.html', '*.js'],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      }
+    },
+    screens:{
+      sm:'640px',
+      md:'768px',
+      lg:'960px',
+      xl:'1200px',
+    },
+    fontFamily:{
+      primary: 'DM Serif Display',
+      secondary: 'Jost'
+    },
+    backgroundImage:{
+      hero:'url(/assets/hero/bg.jpg)',
+      grid:'url(/assets/grid.png)',
+    },
+    extend: {
+      colors:{
+        primary:{
+          DEFAULT: '#292f36',
+          hover: '#343e4a'
+        },
+        secondary:'#4d5053',
+        accent:{
+          DEFAULT:'#cda274',
+          secondary:'#f4f0ec',
+          hover:'#b88c5d' 
+        }
+      }
+    },
   },
   plugins: [],
-}
-
+};
